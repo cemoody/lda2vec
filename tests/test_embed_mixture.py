f117df54dd85909418844ed0b46c4b6efc3af737
Christopher Erick Moody <chrisemoody@gmail.com>
2015-12-24 11:17:19 -0800

    All tests passing

--- tests/test_embed_mixture.py
+++ tests/test_embed_mixture.py
@@ -9,13 +9,13 @@ def softmax(v):
 
 
 def test_embed_mixture():
-    """ Manually test """
+    """ Manually test the projection logic between topic weights and vectors"""
     # Ten documents, two topics, five hidden dimensions
     em = EmbedMixture(10, 2, 5)
     doc_ids = Variable(np.arange(1, dtype='int32'))
-    doc_vector = em(doc_ids)
+    doc_vector = em(doc_ids).data
     # weights -- (n_topics)
     weights = softmax(em.weights.W.data[0, :])
     # (n_hidden) = (n_topics) . (n_topics, n_hidden)
-    doc_vector_test = weights.T * em.factors.W.data
-    assert np.all_close(doc_vector, doc_vector_test)
+    test = np.sum(weights * em.factors.W.data.T, axis=1)
+    assert np.allclose(doc_vector, test)
