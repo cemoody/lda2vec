94694504a2cdf9969db739a612b346ba28879f7a
Christopher Erick Moody <chrisemoody@gmail.com>
2015-12-28 17:03:13 -0800

    Adding more tests for LDA2vec

--- tests/test_lda2vec.py
+++ tests/test_lda2vec.py
@@ -2,34 +2,49 @@ from lda2vec import LDA2Vec
 import numpy as np
 
 
-def single_component(name=None):
-    n_words = 10
-    n_docs = 300
-    n_sent_length = 5
-    n_hidden = 8
-    n_topics = 2
+def generate(n_docs=300, n_words=10, n_sent_length=5, n_hidden=8):
     words = np.random.randint(n_words, size=(n_docs, n_sent_length))
-    doc_ids = np.arange(n_docs)
     _, counts = np.unique(words, return_counts=True)
     model = LDA2Vec(n_words, n_sent_length, n_hidden, counts)
-    model.add_component(n_docs, n_topics, name=name)
+    return model, words
+
+
+def component(model, words, name=None, n_comp=1):
+    n_topics = 2
+    n_docs = words.shape[0]
+    doc_ids = np.arange(n_docs)
+    model = generate()
+    perp_orig = model.calculate_log_perplexity(words, components=[doc_ids])
+    for j in range(n_comp):
+        if name:
+            name += str(j)
+        model.add_component(n_docs, n_topics, name=name)
     # Test perplexity decreases
     model.fit_partial(words, 1.0, components=[doc_ids])
+    perp_fit = model.calculate_log_perplexity(words, components=[doc_ids])
+    msg = "Perplexity should improve with a fit model"
+    assert perp_fit < perp_orig, msg
     del model
 
 
 def test_single_component_no_name():
-    single_component()
-    single_component(name="named_layer")
+    model, words = generate()
+    component(model, words)
 
 
-def test_multiple_component():
-    pass
+def test_single_component_named():
+    model, words = generate()
+    component(model, words, name="named_layer")
 
 
-def test_single_component_target():
-    # Test G/CPU
-    pass
+def test_multiple_components_no_names():
+    model, words = generate()
+    component(model, words, n_comp=2)
+
+
+def test_multiple_components_named():
+    model, words = generate()
+    component(model, words, name="named_layer", n_comp=2)
 
 
 def test_visualization():
@@ -38,6 +53,5 @@ def test_visualization():
 # Test for component w/ & w/o loss type
 # Test over all loss types
 # Test prior / word / target losses
-# Test unnamed components
 # Test raise error for zero components
 # Test raise error for uninitialized components
