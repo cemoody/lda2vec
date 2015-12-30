38154f0b8974b1a92a8b2824abb478f7a1a343b7
Christopher Erick Moody <chrisemoody@gmail.com>
2015-12-29 16:53:05 -0800

    Added pruning in LDA example

--- examples/twenty_newsgroups/lda.py
+++ examples/twenty_newsgroups/lda.py
@@ -13,7 +13,16 @@ texts = fetch_20newsgroups(subset='train').data
 texts = [unicode(d) for d in texts]
 
 max_length = max(len(doc) for doc in texts)
-data, counts, vocab = preprocess(texts, max_length)
+data, counts, vocab = preprocess.tokenize(texts, max_length, tag=False,
+                                          parse=False, entity=False)
+
+# We'll replace very rare words with the pad. The spaCy indices
+# roughly correspond to word frequency in English, so we here we drop
+# words less frequent than the first 20,000
+data[data > 20e3] = -1
+counts = {k: v for k, v in counts.items() if k < 20e3}
+vocab = {k: v for k, v in vocab.items() if k < 20e3}
+
 
 # Number of documents
 n_docs = len(texts)
@@ -21,6 +30,7 @@ n_docs = len(texts)
 n_words = data.max() + 1
 # Number of dimensions in a single word vector
 n_hidden = 128
+# Number of topics to fit
 n_topics = 10
 # Number of times to pass through the data
 epochs = 5
