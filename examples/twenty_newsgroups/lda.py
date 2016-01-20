d723e7d23a4a7eed85a4468cb299c42d6f67a5d4
Christopher Erick Moody <chrisemoody@gmail.com>
2016-01-20 13:36:32 -0800

    Fixed the example to work with skipgram lda2vec

--- examples/twenty_newsgroups/lda.py
+++ examples/twenty_newsgroups/lda.py
@@ -7,9 +7,14 @@
 from lda2vec import preprocess, LDA2Vec, Corpus
 from sklearn.datasets import fetch_20newsgroups
 from chainer import serializers
+from chainer import cuda
 import numpy as np
 import os.path
 import logging
+
+# Optional: moving the model to the GPU makes it ~10x faster
+# set to False if you're having problems with Chainer and CUDA
+gpu = cuda.available
 
 logging.basicConfig()
 
@@ -19,7 +24,7 @@ texts = fetch_20newsgroups(subset='train').data
 texts = [unicode(d) for d in texts]
 
 # Preprocess data
-max_length = 10000   # Limit of 10k words per document
+max_length = 1000   # Limit of 1k words per document
 tokens, vocab = preprocess.tokenize(texts, max_length, tag=False,
                                     parse=False, entity=False)
 corpus = Corpus()
@@ -49,7 +54,7 @@ n_words = flattened.max() + 1
 # Number of dimensions in a single word vector
 n_hidden = 128
 # Number of topics to fit
-n_topics = 20
+n_topics = 5
 # Get the count for each key
 counts = corpus.keys_counts[:n_words]
 # Get the string representation for every compact key
@@ -57,19 +62,22 @@ words = corpus.word_list(vocab)[:n_words]
 
 # Fit the model
 model = LDA2Vec(n_words, n_hidden, counts)
-model.add_categorical_feature(n_docs, n_topics, name='document id')
+model.add_categorical_feature(n_docs, n_topics, name='document_id')
 model.finalize()
-# if os.path.exists('model.hdf5'):
-#     serializers.load_hdf5('model.hdf5', model)
-# Optional: moving the model to the GPU makes it ~50x faster
-model.to_gpu()
+if os.path.exists('model.hdf5'):
+    serializers.load_hdf5('model.hdf5', model)
 for _ in range(20):
-    model.top_words_per_topic('document id')
-    model.fit(flattened, categorical_features=[doc_ids], fraction=1e-2, epochs=1)
+    model.top_words_per_topic('document_id', words)
+    if gpu:
+        model.to_gpu()
+    model.fit(flattened, categorical_features=[doc_ids], fraction=1e-3,
+              epochs=3)
+    if gpu:
+        model.to_cpu()
+model.top_words_per_topic('document_id', words)
 serializers.save_hdf5('model.hdf5', model)
 
 # Visualize the model -- look at lda.ipynb to see the results
 model.to_cpu()
 topics = model.prepare_topics('document id', words)
 np.savez('topics.pyldavis', **topics)
-
