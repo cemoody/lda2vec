27f977bd29a9e631a99ce9d9acc71cf10b74a2e4
intohole <intoblack86@gmail.com>
2016-01-22 17:13:14 +0800

    when i install setup.py , my computer not found package , so i change setup file , and reinstall again , it's work

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
-setup(name='lda2vec',
-      version='0.1',
-      description='Tools for interpreting natural language',
-      author='Christopher E Moody',
-      author_email='chrisemoody@gmail.com',
-      install_requires=install_requires,
-      url='')
+kw = dict(
+    name='lda2vec',
+    version='0.1',
+    description='Tools for interpreting natural language',
+    author='Christopher E Moody',
+    author_email='chrisemoody@gmail.com',
+    install_requires=install_requires,
+    url='',
+    packages = find_packages(),
+)
+
+setup(**kw)
