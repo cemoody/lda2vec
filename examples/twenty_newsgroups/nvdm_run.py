b75367b676137ff4a3c42a134cd51ef395285a3d
Ubuntu <ubuntu@ip-10-97-180-202.ec2.internal>
2016-03-25 05:21:27 +0000

    Updated to the NVDM model

--- examples/twenty_newsgroups/nvdm_run.py
+++ examples/twenty_newsgroups/nvdm_run.py
@@ -6,6 +6,7 @@
 import os.path
 import pickle
 import time
+import random
 
 from chainer import serializers
 import chainer.optimizers as O
@@ -23,11 +24,21 @@ pruned[pruned == 0] = -1
 # Remove bow counts on the first two tokens, which <SKIP> and <EOS>
 bow[:, :2] = 0
 
+# Split up documents into chunks
+chunksize = 10000
+batchsize = 2
+chunks = []
+for b, p in utils.chunks(batchsize, bow, pruned[:, :9000]):
+    for chunk, in utils.chunks(chunksize, p.T):
+        if np.all(chunk == -1):
+            continue
+        flattened = chunk.T.flatten()
+        chunks.append((b, flattened))
+
 # Number of unique words in the vocabulary
 n_vocab = pruned.max() + 1
 # Number of dimensions in a single word vector
 n_units = 256
-batchsize = 176
 n_topics = 20
 counts = corpus.keys_counts[:n_vocab]
 # Get the string representation for every compact key
@@ -44,25 +55,22 @@ optimizer.setup(model)
 j = 0
 fraction = batchsize * 1.0 / pruned.shape[0]
 for epoch in range(500):
-    for b, p in utils.chunks(batchsize, bow, pruned):
-        for chunk, in utils.chunks(100, p.T):
-            if np.all(chunk == -1):
-                continue
-            t0 = time.time()
-            flattened = chunk.T.flatten()
-            rec, kl, dl = model.fit(b, flattened)
-            optimizer.zero_grads()
-            l = rec + kl + dl
-            l.backward()
-            optimizer.update()
-            msg = ("J:{j:05d} E:{epoch:05d} L:{loss:1.3e} "
-                   "P:{prior:1.3e} DL:{dl:1.3e} R:{rate:1.3e}")
-            l.to_cpu()
-            t1 = time.time()
-            dt = t1 - t0
-            rate = batchsize / dt
-            logs = dict(loss=float(rec.data), epoch=epoch, j=j,
-                        prior=float(kl.data), rate=rate, dl=float(dl.data))
-            print msg.format(**logs)
-            j += 1
+    random.shuffle(chunks)
+    for b, flattened in chunks:
+        t0 = time.time()
+        rec, kl, dl = model.fit(b, flattened)
+        optimizer.zero_grads()
+        l = rec + kl + dl
+        l.backward()
+        optimizer.update()
+        msg = ("J:{j:05d} E:{epoch:05d} L:{loss:1.3e} "
+               "P:{prior:1.3e} DL:{dl:1.3e} R:{rate:1.3e}")
+        l.to_cpu()
+        t1 = time.time()
+        dt = t1 - t0
+        rate = batchsize / dt
+        logs = dict(loss=float(rec.data), epoch=epoch, j=j,
+                    prior=float(kl.data), rate=rate, dl=float(dl.data))
+        print msg.format(**logs)
+        j += 1
     serializers.save_hdf5("nvdm.hdf5", model)
