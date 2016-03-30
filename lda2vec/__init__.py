f32a42ae1cdf4d5c776730de39bf322222ea1d85
Christopher Erick Moody <chrisemoody@gmail.com>
2016-03-29 22:44:42 -0700

    Added simple LDA BoW model

--- lda2vec/__init__.py
+++ lda2vec/__init__.py
@@ -5,9 +5,12 @@ import tracking
 import preprocess
 import corpus
 import utils
+import topics
 
 dirichlet_likelihood = dirichlet_likelihood.dirichlet_likelihood
 EmbedMixture = embed_mixture.EmbedMixture
 Tracking = tracking.Tracking
 tokenize = preprocess.tokenize
 Corpus = corpus.Corpus
+prepare_topics = topics.prepare_topics
+print_top_words_per_topic = topics.print_top_words_per_topic
